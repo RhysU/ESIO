8ca240e4cc2e7830e06279ac7a9a42886e743133
Rhys Ulerich <rhys@ices.utexas.edu>
2010-10-02 16:06:52 +0000

    esio: remove compile warnings from sample
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@14569 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/hellompi_f90.f90
+++ apps/hellompi_f90.f90
@@ -1,10 +1,17 @@
-      program hello
-      include 'mpif.h'
-      integer rank, size, ierror, tag, status(MPI_STATUS_SIZE)
+program hello
 
-      call MPI_INIT(ierror)
-      call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
-      call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
-      print*, 'node', rank, ': Hello world'
-      call MPI_FINALIZE(ierror)
-      end
+  use mpi
+
+  implicit none
+
+  integer rank, size, ierror
+
+  call MPI_INIT(ierror)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
+  call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
+
+  print *, 'node', rank, ': Hello world'
+
+  call MPI_FINALIZE(ierror)
+
+end
