34692ed7ce93dc8555ceee9da4dfefa8b906d5d8
Rhys Ulerich <rhys@ices.utexas.edu>
2010-10-13 19:46:59 +0000

    esio: enable parallel tests in buildbot
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@14913 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ tests/field_float.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Must use mpiexec to run serial-but-MPI-enabled tests on some MPI stacks.  In
+# particular, mvapich seems to exhibit this problem.  Moreover, we cannot
+# always use mpiexec on some login nodes.  Better to warn the user that a test
+# was skipped then worry them when make check fails as a result.
+
+if ! [ -x field_float ]; then
+    echo "field_float binary not found or not executable"
+    exit 1
+fi
+
+if ! which mpiexec > /dev/null ; then
+    echo "WARNING: Unable to find mpiexec; skipping field_float"
+    exit 0
+fi
+
+set -e # Fail on first error
+for cmd in "mpiexec -np 1 ./field_float -p 11 -u 13" \
+           "mpiexec -np 2 ./field_float -p  5 -u  7" \
+           "mpiexec -np 3 ./field_float -p  7 -u  5"
+do
+    echo $cmd
+    $cmd
+done
