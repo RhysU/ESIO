eb0ea5869f17b6fd0dda7fbe2b3160e8bce762b7
Rhys Ulerich <rhys@ices.utexas.edu>
2010-11-03 06:25:01 +0000

    esio: add handle/files/strings doxygen per #1110
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@15467 0bd85f0b-c151-43b9-a1e9-27231104551f

--- esio/esio.h
+++ esio/esio.h
@@ -23,8 +23,6 @@
 //-----------------------------------------------------------------------el-
 // $Id$
 
-/* TODO Document C API */
-
 #ifndef __ESIO_ESIO_H
 #define __ESIO_ESIO_H
 
@@ -32,36 +30,116 @@
 
 /** @file
  * Provides ESIO's C-based public API following the library's
- * \ref concepts "usage concepts".
+ * \ref concepts "usage concepts".  All methods in this file
+ * invoke ESIO's \ref errorC "error handling" mechanisms on failure.
  */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** An opaque type following ESIO's \ref conceptshandles "handle concept". */
 typedef struct esio_state_s *esio_state;
 
-/** \name Initializing and finalizing a state handle */
+/**
+ * \name Initializing and finalizing a state handle
+ * See \ref conceptshandles "handles" for the associated semantics.
+ */
 /*\@{*/
 
+/**
+ * Initialize a handle against the given MPI communicator.
+ * The handle must be finalized using esio_finalize() to avoid
+ * resource leaks.
+ *
+ * \param comm MPI communicator (e.g. \c MPI_COMM_WORLD) used to determine
+ *             the parallel scope of the handle.  All ESIO calls employing
+ *             the handle must be made collectively on \c comm.
+ * \return A new handle on success.  Otherwise \c NULL.
+ */
 esio_state esio_initialize(MPI_Comm comm);
+
+/**
+ * Finalize a handle.
+ *
+ * \param s Handle to finalize.  May be \c NULL.
+ * \return ESIO_SUCCESS \c (0) on success or
+ *         one of ::esio_error_status on failure.
+ */
 int esio_finalize(esio_state s);
 /*\@}*/
 
 
-/** \name Opening and closing data files */
+/** \name Opening and closing files */
 /*\@{*/
+
+/**
+ * Create a new file or overwrite an existing one.
+ *
+ * \param s Handle to use.
+ * \param file Name of the file to open.
+ * \param overwrite If zero, fail if an existing file is detected.
+ *                  If nonzero, clobber any existing file.
+ *
+ * \return ESIO_SUCCESS \c (0) on success or
+ *         one of ::esio_error_status on failure.
+ */
 int esio_file_create(esio_state s, const char *file, int overwrite);
+
+/**
+ * Open an existing file.
+ *
+ * \param s Handle to use.
+ * \param file Name of the file to open.
+ * \param readwrite If zero, open the file in read-only mode.
+ *                  If nonzero, open the file in read-write mode.
+ *
+ * \return ESIO_SUCCESS \c (0) on success or
+ *         one of ::esio_error_status on failure.
+ */
 int esio_file_open(esio_state s, const char *file, int readwrite);
+
+/**
+ * Close the currently open file.
+ *
+ * \param s Handle to use.
+ *
+ * \return ESIO_SUCCESS \c (0) on success or
+ *         one of ::esio_error_status on failure.
+ */
 int esio_file_close(esio_state s);
 /*\@}*/
 
 
 /** \name Manipulating string-valued attributes */
 /*\@{*/
+
+/**
+ * Set the attribute \c name to be \c value.
+ * Any existing attribute will be overwritten.
+ *
+ * @param s Handle to use.
+ * @param name Null-terminated attribute name.
+ * @param value Null-terminated attribute value.
+ *
+ * \return ESIO_SUCCESS \c (0) on success or
+ *         one of ::esio_error_status on failure.
+ */
 int esio_string_set(const esio_state s,
                     const char *name,
                     const char *value);
+
+/**
+ * Get the null-terminated string associated with attribute \c name.  The
+ * routine allocates sufficient storage to hold the string and returns it.  The
+ * caller must <tt>free</tt> the memory to avoid resource leaks.
+ *
+ * @param s Handle to use.
+ * @param name Null-terminated attribute name.
+ *
+ * @return A newly allocated buffer containing the value on success.
+ *         \c NULL on failure.
+ */
 char* esio_string_get(const esio_state s,
                       const char *name);
 /*\@}*/
