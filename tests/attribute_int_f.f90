126809987188455c0769e8c79cf635c7dcced369
Rhys Ulerich <rhys@ices.utexas.edu>
2010-12-08 22:54:56 +0000

    esio: #1116 add attribute_int Fortran tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@16185 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/attribute_int_f.f90
+++ tests/attribute_int_f.f90
@@ -23,6 +23,8 @@
 !!-----------------------------------------------------------------------el-
 !! $Id$
 
+#include "testframework_assert.h"
+
 program attribute_int_f
 
     use mpi
@@ -31,9 +33,42 @@ program attribute_int_f
 
     implicit none
 
+    integer, parameter :: value_scalar    = 123
+    integer, parameter :: value_vector(4) = (/12, 34, 56, 78/)
+    integer            :: buffer(4), i
+
     call testframework_setup()
 
-    write (output,*) 'FIXME: Implement Fortran test'
+!   Create a file, write values, and close it
+    call esio_file_create(h, filename, .false., ierr)
+    ASSERT(ierr == 0)
+    call esio_attribute_write_integer(h, "name_scalar", value_scalar, ierr)
+    ASSERT(ierr == 0)
+    call esio_attribute_writev_integer(h, "name_vector", value_vector, &
+                                       size(value_vector), ierr)
+    ASSERT(ierr == 0)
+    call esio_file_close(h, ierr)
+    ASSERT(ierr == 0)
+
+!   Re-open the file read-only, check sizes and values, and close it
+    call esio_file_open(h, filename, .false., ierr)
+    ASSERT(ierr == 0)
+    call esio_attribute_sizev(h, "name_scalar", buffer(1), ierr)
+    ASSERT(buffer(1) == 1)
+    call esio_attribute_read_integer(h, "name_scalar", buffer(1), ierr)
+    ASSERT(ierr == 0)
+    ASSERT(buffer(1) == 123)
+
+    call esio_attribute_sizev(h, "name_vector", buffer(1), ierr)
+    ASSERT(buffer(1) == size(value_vector))
+    call esio_attribute_readv_integer(h, "name_vector", buffer, &
+                                      size(value_vector), ierr)
+    ASSERT(ierr == 0)
+    do i = 1, size(value_vector)
+        ASSERT(buffer(i) == value_vector(i))
+    end do
+    call esio_file_close(h, ierr)
+    ASSERT(ierr == 0)
 
     call testframework_teardown()
 
