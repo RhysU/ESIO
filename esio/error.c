309fd72c9e9b61acd0aee1b45cabf41c9499d86d
Rhys Ulerich <rhys@ices.utexas.edu>
2010-12-15 17:20:54 +0000

    esio: fix pedigree of error.c
    Include pointer to LGPL permission from Brian Gough
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@16364 0bd85f0b-c151-43b9-a1e9-27231104551f

--- esio/error.c
+++ esio/error.c
@@ -1,22 +1,36 @@
-/*
- * Copyright (C) 1996, 1997, 1998, 1999, 2000, 2007 Gerard Jungman, Brian Gough
- * Adapted from the GNU Scientific Library
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 51
- * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// esio 0.0.1: ExaScale IO library for turbulence simulation restart files
+// http://pecos.ices.utexas.edu/
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
 // $Id$
+
+/******************************************************************************
+ * Functionality adopted from the GNU Scientific Library.
+ * Copyright (C) 1996, 1997, 1998, 1999, 2000, 2007 Gerard Jungman, Brian Gough
+ * Relicensed as LGPL by permission of Brian Gough on 19 Feb 2010:
+ *   "If anyone wants to use the error macro definitions or
+ *   error handler from GSL under the LGPL I am fine with that."
+ * See http://www.mail-archive.com/gsl-discuss@sourceware.org/msg00764.html
+ ******************************************************************************/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
