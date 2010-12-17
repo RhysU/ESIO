3e727b0241e3fc34687f63ee1fbd63bb9b5c1d08
Rhys Ulerich <rhys@ices.utexas.edu>
2010-12-17 22:53:09 +0000

    esio: #1168 document restart API
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@16402 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ examples/restart1.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <mpi.h>
+#include <esio/esio.h>
+
+int main(int argc, char *argv[])
+{
+    /* Initialize MPI */
+    MPI_Init(&argc, &argv);
+    atexit((void (*) ()) MPI_Finalize);
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    /* Create restart file template */
+    esio_handle h = esio_handle_initialize(MPI_COMM_WORLD);
+    esio_file_create(h, "template.h5", 1 /* overwrite */);
+    esio_string_set(h, "program", argv[0]);
+    esio_file_close(h);
+
+    /* Main simulation loop */
+    double example[2];
+    for (int i = 0; i < 10; ++i) {
+
+        /* Update simulation state */
+        example[0] = i * world_rank;
+        example[1] = i * world_rank + 1;
+
+        /* Create an uncommitted restart file based on the template */
+        esio_file_clone(h, "template.h5", "uncommitted.h5", 1 /*overwrite*/);
+
+        /* Save the current simulation state in the uncommitted restart */
+        esio_line_write_double(h, "example", example,
+                               2*world_size, 2*world_rank, 2, 1);
+
+        /* Commit the restart file.  Up to 5 older restarts are retained. */
+        esio_file_close_restart(h, "committed#.h5", 5 /*retain_count*/);
+    }
+
+    esio_handle_finalize(h);
+
+    return 0;
+}
