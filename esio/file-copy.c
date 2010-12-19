660bc2858022c2a54dfb1830eabaf7735e232759
Rhys Ulerich <rhys@ices.utexas.edu>
2010-12-19 03:15:31 +0000

    esio: #1242 file-copy rework
    Still not everything I'd like, but better.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@16414 0bd85f0b-c151-43b9-a1e9-27231104551f

--- esio/file-copy.c
+++ esio/file-copy.c
@@ -1,29 +1,31 @@
-/* Copying of files modified from gnulib's copy-file module.
- * Modified to fit into ESIO's error handling framework.
- *
- * Copyright (C) 2001-2003, 2009-2010 Free Software Foundation, Inc.
- * Written by Bruno Haible <haible@clisp.cons.org>, 2001.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// esio 0.0.1: ExaScale IO library for turbulence simulation restart files
+// http://pecos.ices.utexas.edu/
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// $Id$
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-
-/* Specification.  */
-#include "file-copy.h"
 
 #include <errno.h>
 #include <fcntl.h>
@@ -32,14 +34,13 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include "safe-read.h"
-#include "full-write.h"
 #include "binary-io.h"
+#include "full-write.h"
+#include "safe-read.h"
 
-/* ESIO, not gnulib, error handling */
+/* ESIO's error handling */
 #include "error.h"
-
-enum { IO_SIZE = 32 * 1024 };
+#include "file-copy.h"
 
 int
 file_copy (const char *src_filename,
@@ -47,79 +48,83 @@ file_copy (const char *src_filename,
            int overwrite,
            int blockuntilsync)
 {
-    int src_fd;
-    struct stat statbuf;
-    int mode;
-    int dest_fd;
-    char *buf = malloc (IO_SIZE);
+    /* Allocate working buffer */
+    const int OPSIZE = 32 * 1024;
+    char *buf = malloc(OPSIZE);
     if (!buf) {
         ESIO_ERROR("Unable to allocate buffer for copy operation",
                    ESIO_ENOMEM);
     }
 
-    src_fd = open (src_filename, O_RDONLY | O_BINARY);
-    if (src_fd < 0 || fstat (src_fd, &statbuf) < 0) {
-        ESIO_ERROR("Error opening copy source file for reading",
-                   ESIO_EFAILED);
+    /* Open up source and destination files */
+    const int src = open(src_filename, O_RDONLY | O_BINARY);
+    if (src < 0) {
+        ESIO_ERROR("Error opening copy source for reading", ESIO_EFAILED);
     }
-
-    mode = statbuf.st_mode & 07777;
+    struct stat statbuf;
+    if (fstat(src, &statbuf) < 0) {
+        ESIO_ERROR("Error opening copy source details", ESIO_EFAILED);
+    }
+    const int mode = statbuf.st_mode & 07777;
 
     /* Open a new file or truncate an existing one based on overwrite */
+    int dst;
     if (overwrite) {
-        dest_fd = open (dest_filename,
-                        O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0600);
-        if (dest_fd < 0) {
+        dst = open(dest_filename,
+                   O_BINARY | O_CREAT | O_TRUNC | O_WRONLY, 0600);
+        if (dst < 0) {
             ESIO_ERROR("Error overwriting copy destination file for writing",
                         ESIO_EFAILED);
         }
     } else {
-        dest_fd = open (dest_filename,
-                        O_WRONLY | O_CREAT | O_EXCL | O_BINARY, 0600);
-        if (dest_fd < 0) {
+        dst = open(dest_filename,
+                   O_BINARY | O_CREAT | O_EXCL | O_WRONLY, 0600);
+        if (dst < 0) {
             ESIO_ERROR("Error opening new copy destination file for writing",
                        ESIO_EFAILED);
         }
     }
 
-    /* Copy the file contents.  */
-    for (;;) {
-        size_t n_read = safe_read (src_fd, buf, IO_SIZE);
-        if (n_read == SAFE_READ_ERROR) {
+    /* Copy file contents in chunks of size OPSIZE */
+    while (1) {
+        const size_t n = safe_read(src, buf, OPSIZE);
+        if (n == SAFE_READ_ERROR) {
             ESIO_ERROR("Error reading source file", ESIO_EFAILED);
         }
-        if (n_read == 0) {
+        if (n == 0) {
             break;
         }
 
-        if (full_write (dest_fd, buf, n_read) < n_read) {
+        if (full_write(dst, buf, n) < n) {
             ESIO_ERROR("Error writing destination file", ESIO_EFAILED);
         }
     }
 
-    free (buf);
+    /* Done with temporary buffer */
+    free(buf);
 
     /* If requested, ensure the destination file has hit the device */
     if (blockuntilsync) {
-        if (fsync (dest_fd) < 0) {
+        if (fsync(dst) < 0) {
             ESIO_ERROR("Error sync-ing destination file", ESIO_EFAILED);
         }
     }
 
-    if (close (dest_fd) < 0) {
+    /* Close source and destination file */
+    if (close(dst) < 0) {
         ESIO_ERROR("Error closing destination file", ESIO_EFAILED);
     }
-    if (close (src_fd) < 0) {
+    if (close(src) < 0) {
         ESIO_ERROR("Error closing source file", ESIO_EFAILED);
     }
 
 #if HAVE_CHOWN
-    /* Preserve the owner and group.  */
-    chown (dest_filename, statbuf.st_uid, statbuf.st_gid);
+    /* Preserve ownership and group  */
+    chown(dest_filename, statbuf.st_uid, statbuf.st_gid);
 #endif
 
-    /* Preserve the access permissions.  */
-    chmod (dest_filename, mode);
+    /* Preserve permissions */
+    chmod(dest_filename, mode);
 
     return ESIO_SUCCESS;
 }
--- esio/file-copy.h
+++ esio/file-copy.h
@@ -1,22 +1,27 @@
-/* Copying of files modified from gnulib's copy-file module.
- * Modified to fit into ESIO's error handling framework.
- *
- * Copyright (C) 2001-2003, 2009-2010 Free Software Foundation, Inc.
- * Written by Bruno Haible <haible@clisp.cons.org>, 2001.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// esio 0.0.1: ExaScale IO library for turbulence simulation restart files
+// http://pecos.ices.utexas.edu/
+//
+// Copyright (C) 2010 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// $Id$
 
 #ifndef __ESIO_FILE_COPY_H
 #define __ESIO_FILE_COPY_H
