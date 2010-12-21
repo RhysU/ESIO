8f244e44a9438073d7ec8bd69926e4799b1c60bc
Rhys Ulerich <rhys@ices.utexas.edu>
2010-12-21 23:19:19 +0000

    esio: #1122 improve parsing logic
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@16434 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/esio_bench.c
+++ apps/esio_bench.c
@@ -31,6 +31,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sysexits.h>
 
 #include "argp.h"
 #include "mpi_argp.h"
@@ -38,7 +39,23 @@
 #include <mpi.h>
 #include <esio/esio.h>
 
-// Argp details: http://www.gnu.org/s/libc/manual/html_node/Argp.html
+//*************************************************************
+// STATIC PROTOTYPES STATIC PROTOTYPES STATIC PROTOTYPES STATIC 
+//*************************************************************
+
+static inline int min(int a, int b);
+
+static void to_human_readable_byte_count(long bytes,
+                                         int si,
+                                         double *coeff,
+                                         const char **units);
+
+static long from_human_readable_byte_count(const char *str);
+
+//*******************************************************************
+// ARGP DETAILS: http://www.gnu.org/s/libc/manual/html_node/Argp.html
+//*******************************************************************
+
 const char *argp_program_version      = "esio_bench " PACKAGE_VERSION;
 const char *argp_program_bug_address  = PACKAGE_BUGREPORT;
 static const char doc[]               = "ESIO Benchmarking Tool" // Brief
@@ -78,9 +95,11 @@ static struct argp_option options[] = {
 // Used by main to communicate with parse_opt
 struct arguments {
     int verbose;
-    int field_cglobal, field_bglobal, field_aglobal, field_dims[3];
-    int plane_bglobal, plane_aglobal, plane_dims[2];
-    int line_aglobal, line_dims[1];
+    int field_cglobal, field_bglobal, field_aglobal;
+    int plane_bglobal, plane_aglobal;
+    int line_aglobal;
+    int field_dims[3], plane_dims[2], line_dims[1];
+    long field_bytes, plane_bytes, line_bytes;
     int ncomponents;
 };
 
@@ -88,14 +107,147 @@ struct arguments {
 static error_t
 parse_opt(int key, char *arg, struct argp_state *state)
 {
-    (void) arg; // Possibly unused
-
     // Get the input argument from argp_parse.
     struct arguments *arguments = state->input;
 
     switch (key) {
         case 'v':
             arguments->verbose = 1;
+            break;
+
+        case 'n':
+            if (1 != sscanf(arg ? arg : "", " %d ",
+                            &(arguments->ncomponents))) {
+                argp_failure(state, EX_USAGE, 0,
+                        "ncomponents option is malformed: '%s'", arg);
+            }
+            if (arguments->ncomponents < 1) {
+                argp_failure(state, EX_USAGE, 0,
+                        "ncomponents value %d must be strictly positive",
+                        arguments->ncomponents);
+            }
+            break;
+
+        case 'f':
+            if (arg) {
+                arguments->field_bytes = from_human_readable_byte_count(arg);
+                if (arguments->field_bytes < 1)
+                    argp_failure(state, EX_USAGE, 0,
+                            "field-memory option is malformed: '%s'", arg);
+            }
+            break;
+
+        case 'p':
+            if (arg) {
+                arguments->plane_bytes = from_human_readable_byte_count(arg);
+                if (arguments->plane_bytes < 1)
+                    argp_failure(state, EX_USAGE, 0,
+                            "plane-memory option is malformed: '%s'", arg);
+            }
+            break;
+
+        case 'l':
+            if (arg) {
+                arguments->line_bytes = from_human_readable_byte_count(arg);
+                if (arguments->line_bytes < 1)
+                    argp_failure(state, EX_USAGE, 0,
+                            "line-memory option is malformed: '%s'", arg);
+            }
+            break;
+
+        case 'F':
+            if (3 != sscanf(arg ? arg : "", " %d x %d x %d ",
+                            &(arguments->field_dims[0]),
+                            &(arguments->field_dims[1]),
+                            &(arguments->field_dims[2]))) {
+                argp_failure(state, EX_USAGE, 0,
+                        "field-dims option is malformed: '%s'", arg);
+            }
+            if (    arguments->field_dims[0] < 0
+                 || arguments->field_dims[1] < 0
+                 || arguments->field_dims[2] < 0) {
+                argp_failure(state, EX_USAGE, 0,
+                        "field-dims values %dx%dx%d must be nonnegative",
+                        arguments->field_dims[0],
+                        arguments->field_dims[1],
+                        arguments->field_dims[2]);
+            }
+            break;
+
+
+        case 'P':
+            if (2 != sscanf(arg ? arg : "", " %d x %d ",
+                            &(arguments->plane_dims[0]),
+                            &(arguments->plane_dims[1]))) {
+                argp_failure(state, EX_USAGE, 0,
+                        "plane-dims option is malformed: '%s'", arg);
+            }
+            if (arguments->plane_dims[0] < 0 || arguments->plane_dims[1] < 0) {
+                argp_failure(state, EX_USAGE, 0,
+                        "plane-dims values %dx%d must be nonnegative",
+                        arguments->plane_dims[0], arguments->plane_dims[1]);
+            }
+            break;
+
+
+        case 'L':
+            if (1 != sscanf(arg ? arg : "", " %d ",
+                            &(arguments->line_dims[0]))) {
+                argp_failure(state, EX_USAGE, 0,
+                        "line-global option is malformed: '%s'", arg);
+            }
+            if (arguments->line_dims[0] < 0) {
+                argp_failure(state, EX_USAGE, 0,
+                        "line-dims value '%d' must be nonnegative",
+                        arguments->line_dims[0]);
+            }
+            break;
+
+        case FIELD_GLOBAL:
+            if (3 != sscanf(arg ? arg : "", " %d x %d x %d ",
+                            &(arguments->field_cglobal),
+                            &(arguments->field_bglobal),
+                            &(arguments->field_aglobal))) {
+                argp_failure(state, EX_USAGE, 0,
+                        "field-global option not of form CxBxA: '%s'", arg);
+            }
+            if (    arguments->field_cglobal < 1
+                 || arguments->field_bglobal < 1
+                 || arguments->field_aglobal < 1) {
+                argp_failure(state, EX_USAGE, 0,
+                        "field-global values %dx%dx%d must be strictly positive",
+                        arguments->field_cglobal,
+                        arguments->field_bglobal,
+                        arguments->field_aglobal);
+            }
+            break;
+
+
+        case PLANE_GLOBAL:
+            if (2 != sscanf(arg ? arg : "", " %d x %d ",
+                            &(arguments->plane_bglobal),
+                            &(arguments->plane_aglobal))) {
+                argp_failure(state, EX_USAGE, 0,
+                        "plane-global option not of form BxA: '%s'", arg);
+            }
+            if (arguments->plane_bglobal < 1 || arguments->plane_aglobal < 1) {
+                argp_failure(state, EX_USAGE, 0,
+                        "plane-global values %dx%d must be strictly positive",
+                        arguments->plane_bglobal, arguments->plane_aglobal);
+            }
+            break;
+
+        case LINE_GLOBAL:
+            if (1 != sscanf(arg ? arg : "", " %d ",
+                            &(arguments->line_aglobal))) {
+                argp_failure(state, EX_USAGE, 0,
+                        "line-global option is malformed: '%s'", arg);
+            }
+            if (arguments->line_aglobal < 0) {
+                argp_failure(state, EX_USAGE, 0,
+                        "line-global value %d must be strictly positive",
+                        arguments->line_aglobal);
+            }
             break;
 
         default:
@@ -109,103 +261,6 @@ static struct argp argp = {
     options, parse_opt, args_doc, doc,
     0 /*children*/, 0 /*help_filter*/, 0 /*argp_domain*/
 };
-
-static inline int min(int a, int b)
-{
-    return a < b ? a : b;
-}
-
-// Adapted from http://stackoverflow.com/questions/3758606/
-// how-to-convert-byte-size-into-human-readable-format-in-java
-static void to_human_readable_byte_count(long bytes,
-                                         int si,
-                                         double *scaled,
-                                         const char **units)
-{
-    // Static lookup table of byte-based SI units
-    // Looking ahead, 'H' is for the unofficial 'hella' = 10^27.  :P
-    static const char *suffix[][2] = { { "B",  "B"   },
-                                       { "kB", "KiB" },
-                                       { "MB", "MiB" },
-                                       { "GB", "GiB" },
-                                       { "TB", "TiB" },
-                                       { "EB", "EiB" },
-                                       { "ZB", "ZiB" },
-                                       { "YB", "YiB" },
-                                       { "HB", "HiB" } };
-    const int unit = si ? 1000 : 1024;
-    const int exp  = min( (int) (log(bytes) / log(unit)),
-                          (int) sizeof(suffix) / sizeof(suffix[0]) );
-    *scaled = bytes / pow(unit, exp);
-    *units  = suffix[exp][!!si];
-}
-
-// Convert strings like the following into byte counts
-//    5MB, 5 MB, 5M, 3.7GB, 123b, 456kBytes
-// with some amount of forgiveness baked into the parsing.
-static long from_human_readable_byte_count(const char *str)
-{
-    // Parse leading numeric factor
-    char *endptr;
-    errno = 0;
-    const double scaled = strtod(str, &endptr);
-    if (errno) return -1;
-
-    // Skip any intermediate white space
-    while (isspace(*endptr++)) /* empty */ ;
-
-    // Read off first character which should be an SI prefix
-    int exp  = 0;
-    int unit = 1024;
-    switch (toupper(*endptr)) {
-        case 'B':  exp =  0; break;
-        case 'K':  exp =  3; break;
-        case 'M':  exp =  6; break;
-        case 'G':  exp =  9; break;
-        case 'T':  exp = 12; break;
-        case 'E':  exp = 15; break;
-        case 'Z':  exp = 18; break;
-        case 'Y':  exp = 21; break;
-        case 'H':  exp = 24; break;
-
-        case ' ':
-        case '\t':
-        case '\0': exp =  0; goto done;
-
-        default:   return -1;
-    }
-    ++endptr;
-
-    // If an 'i' or 'I' is present use SI factor-of-1000 units
-    if (toupper(*endptr) == 'I') {
-        ++endptr;
-        unit = 1000;
-    }
-
-    // Next character should either be 'B' or empty/whitespace
-    switch (toupper(*endptr)) {
-        case 'B':
-        case ' ':
-        case '\t':
-        case '\0': goto done;
-
-        default:   return -1;
-    }
-    ++endptr;
-
-    // Next character must be empty/whitespace
-    switch (*endptr) {
-        case ' ':
-        case '\t':
-        case '\0': goto done;
-
-        default:   return -1;
-    }
-
-done:
-    return scaled * pow(unit, exp);
-}
-
 
 int main(int argc, char *argv[])
 {
@@ -224,6 +279,25 @@ int main(int argc, char *argv[])
     // Parse arguments
     mpi_argp_parse(world_rank, &argp, argc, argv, 0, 0, &arguments);
 
+    // DEBUG: Dump arguments
+    printf("verbose:      %d\n", arguments.verbose);
+    printf("ncomponents:  %d\n", arguments.ncomponents);
+    printf("field_global: %d x %d x %d\n", arguments.field_cglobal,
+                                           arguments.field_bglobal,
+                                           arguments.field_aglobal);
+    printf("field_dims:   %d x %d x %d\n", arguments.field_dims[0],
+                                           arguments.field_dims[1],
+                                           arguments.field_dims[2]);
+    printf("field_bytes:  %ld\n", arguments.field_bytes);
+    printf("plane_global: %d x %d\n", arguments.plane_bglobal,
+                                      arguments.plane_aglobal);
+    printf("plane_dims:   %d x %d\n", arguments.plane_dims[0],
+                                      arguments.plane_dims[1]);
+    printf("plane_bytes:  %ld\n", arguments.plane_bytes);
+    printf("line_global:  %d\n", arguments.line_aglobal);
+    printf("line_dims:    %d\n", arguments.line_dims[0]);
+    printf("line_bytes:   %ld\n", arguments.line_bytes);
+
 /*     esio_handle h = esio_handle_initialize(MPI_COMM_WORLD); */
 /*     esio_file_create(h, "data.h5", 1 |+ overwrite +|); */
 
@@ -240,4 +314,96 @@ int main(int argc, char *argv[])
 /*     esio_handle_finalize(h); */
 
     return 0;
+}
+
+static inline int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+
+// Adapted from http://stackoverflow.com/questions/3758606/
+// how-to-convert-byte-size-into-human-readable-format-in-java
+static void to_human_readable_byte_count(long bytes,
+                                         int si,
+                                         double *coeff,
+                                         const char **units)
+{
+    // Static lookup table of byte-based SI units
+    static const char *suffix[][2] = { { "B",  "B"   },
+                                       { "kB", "KiB" },
+                                       { "MB", "MiB" },
+                                       { "GB", "GiB" },
+                                       { "TB", "TiB" },
+                                       { "EB", "EiB" },
+                                       { "ZB", "ZiB" },
+                                       { "YB", "YiB" } };
+    const int unit = si ? 1000 : 1024;
+    const int exp  = min( (int) (log(bytes) / log(unit)),
+                          (int) sizeof(suffix) / sizeof(suffix[0]) );
+    *coeff = bytes / pow(unit, exp);
+    *units  = suffix[exp][!!si];
+}
+
+
+// Convert strings like the following into byte counts
+//    5MB, 5 MB, 5M, 3.7GB, 123b, 456kBytes
+// with some amount of forgiveness baked into the parsing.
+static long from_human_readable_byte_count(const char *str)
+{
+    // Parse leading numeric factor
+    char *endptr;
+    errno = 0;
+    const double coeff = strtod(str, &endptr);
+    if (errno) return -1;
+
+    // Skip any intermediate white space
+    while (isspace(*endptr)) ++endptr;
+
+    // Read off first character which should be an SI prefix
+    int exp  = 0;
+    int unit = 1024;
+    switch (toupper(*endptr)) {
+        case 'B':  exp =  0; break;
+        case 'K':  exp =  3; break;
+        case 'M':  exp =  6; break;
+        case 'G':  exp =  9; break;
+        case 'T':  exp = 12; break;
+        case 'E':  exp = 15; break;
+        case 'Z':  exp = 18; break;
+        case 'Y':  exp = 21; break;
+
+        case ' ':
+        case '\t':
+        case '\0': exp =  0; goto done;
+
+        default:   return -1;
+    }
+    ++endptr;
+
+    // If an 'i' or 'I' is present use SI factor-of-1000 units
+    if (toupper(*endptr) == 'I') {
+        ++endptr;
+        unit = 1000;
+    }
+
+    // Next character must be one of B/empty/whitespace
+    switch (toupper(*endptr)) {
+        case 'B':
+        case ' ':
+        case '\t': ++endptr;  break;
+
+        case '\0': goto done;
+
+        default:   return -1;
+    }
+
+    // Skip any remaining white space
+    while (isspace(*endptr)) ++endptr;
+
+    // Parse error on anything but a null terminator
+    if (*endptr) return -1;
+
+done:
+    return exp ? coeff * pow(unit, exp / 3) : coeff;
 }
