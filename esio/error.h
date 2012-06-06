6a9e02d878bc68b959b25d3030a29ce828982797
Rhys Ulerich <rhys@ices.utexas.edu>
2012-06-06 22:08:10 +0000

    suzerain: correct ESIO_MPICHKx_TEMPLATE
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@30676 0bd85f0b-c151-43b9-a1e9-27231104551f

--- esio/error.h
+++ esio/error.h
@@ -261,8 +261,8 @@ esio_set_stream(FILE * new_stream) ESIO_API;
     do { \
         const int _chk_stat = (stmt); \
         if (_chk_stat != MPI_SUCCESS) { \
-            char _chk_reason[255]; \
-            char *_chk_mpistring = NULL; \
+            char _chk_reason[384]; \
+            char _chk_mpistring[MPI_MAX_ERROR_STRING]; \
             int _chk_len; \
             const int _chk_string_stat \
                 = MPI_Error_string(_chk_stat,_chk_mpistring,&_chk_len); \
