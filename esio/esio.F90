e7376acade78613cf094d471fe6a6a507f42466c
Rhys Ulerich <rhys@ices.utexas.edu>
2013-01-09 04:36:59 +0000

    esio: #2680 update headers to be LGPL3
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/esio/trunk@35720 0bd85f0b-c151-43b9-a1e9-27231104551f

--- esio/esio.F90
+++ esio/esio.F90
@@ -10,7 +10,7 @@
 !!
 !! ESIO is free software: you can redistribute it and/or modify
 !! it under the terms of the GNU Lesser General Public License as published
-!! by the Free Software Foundation, either version 2.1 of the License, or
+!! by the Free Software Foundation, either version 3.0 of the License, or
 !! (at your option) any later version.
 !!
 !! ESIO is distributed in the hope that it will be useful,
